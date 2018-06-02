2682c97998e0c6efdc47d936ef2645808e8c39d0
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-02 12:34:19 +0200

    initiates connection with server

--- src/containers/ProjectManager/ProjectManager.js
+++ src/containers/ProjectManager/ProjectManager.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class ProjectManager extends Component {
+    state = {
+        beneficiaries: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8080/beneficiaries')
+            .then(response => {
+                const beneficiaries = response.data;
+                this.setState({beneficiaries : beneficiaries});
+                console.log(beneficiaries);
+            });
+    }
+
     render() {
+        let beneficiaryOutput = <p>Brak beneficjentów w bazie danych</p>
+        if (this.state.beneficiaries.length > 0) {
+            console.log(this.state.beneficiaries[0].name);
+            beneficiaryOutput = <p>Beneficjenci {this.state.beneficiaries[0].name}</p>
+        }
         return (
+
             <div>
-                <p>Beneficjenci</p>
+                {beneficiaryOutput}
                 <p>Projekty</p>
                 <p>Zadania</p>
             </div>
