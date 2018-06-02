9eba8afc28efa6e8e3a367b46afdf7a92cfec74d
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-02 12:08:32 +0200

    initiates ProjectManager container

---
+++ src/containers/ProjectManager/ProjectManager.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ProjectManager extends Component {
+    render() {
+        return (
+            <div>
+                <p>Beneficjenci</p>
+                <p>Projekty</p>
+                <p>Zadania</p>
+            </div>
+
+        )
+    }
+} 
+
+export default ProjectManager;
