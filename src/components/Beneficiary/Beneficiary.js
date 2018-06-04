257a354ee7c803731fd36004211cecbe23022105
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-04 09:39:14 +0200

    develops chosen beneficiary

--- src/components/Beneficiary/Beneficiary.js
+++ src/components/Beneficiary/Beneficiary.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const beneficiary = (props) => (
-    <div className='card bg-info'>
+    <div className='card text-center'>
         <div className="card-header">nazwa: {props.name}</div>
+        <div className="card-body">
+            <p className="card-text">@jakieś informacje o beneficjencie@</p>
+            <a className="btn btn-danger">Usuń beneficjenta</a>
+            <a className="btn btn-warning">Edytuj beneficjenta</a>
+        </div>
+        <div className="card-footer text-muted">@Stopka z innymi informacjami@</div>
     </div>
 )
 
