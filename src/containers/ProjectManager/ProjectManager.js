b387afc5275a043a5cb2d377267e0467697aadbf
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-04 14:50:55 +0200

    develops project selection on click

--- src/containers/ProjectManager/ProjectManager.js
+++ src/containers/ProjectManager/ProjectManager.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 
 import NavigationPanel from '../../components/NavigationPanel/NavigationPanel';
 import SelectedBeneficiary from '../../components/SelectedComponents/SelectedBeneficiary';
+import Project from '../../components/Projects/Project/Project';
 
 class ProjectManager extends Component {
     state = {
         beneficiaries: [],
         previousBeneficiary: [],
         selectedBeneficiary: null,
-        beneficiaryProjects: []
+        beneficiaryProjects: [],
+        selectedProject: null
     }
 
     componentDidMount() {
@@ -22,22 +24,35 @@ class ProjectManager extends Component {
 
     componentDidUpdate() {
         if (this.state.selectedBeneficiary !== null && (this.state.selectedBeneficiary !== this.state.previousBeneficiary)) {
-                axios.get('http://localhost:8080/projects/beneficiary/' + this.state.selectedBeneficiary.id)
-                    .then(response => {
-                        this.setState({ beneficiaryProjects: response.data });
-                    });
+            axios.get('http://localhost:8080/projects/beneficiary/' + this.state.selectedBeneficiary.id)
+                .then(response => {
+                    this.setState({ beneficiaryProjects: response.data });
+                });
             const previousBeneficiary = this.state.selectedBeneficiary;
             this.setState({
                 previousBeneficiary: previousBeneficiary
             });
         }
+
+        // if (this.state.selectedProject !== null && (this.state.selectedProject !== this.state.previousProject)) {
+        //     axios.get('http://localhost:8080/projects/' + this.state.selectedProject.id)
+        //         .then(response => {
+        //             // this.setState({ beneficiaryProjects: response.data });
+        //             console.log(response.data);
+        //         });
+        //     const previousProject = this.state.selectedProject;
+        //     this.setState({
+        //         previousProject: previousProject
+        //     });
+        // }
     }
 
     selectedBeneficiaryHandler = (beneficiary) => {
         const previousBeneficiary = this.state.selectedBeneficiary;
         this.setState({
             previousBeneficiary: previousBeneficiary,
-            selectedBeneficiary: beneficiary
+            selectedBeneficiary: beneficiary,
+            selectedProject: null
         });
     }
 
@@ -45,12 +60,24 @@ class ProjectManager extends Component {
         console.log(project);
         const previousProject = this.state.selectedProject;
         this.setState({
-            previousProject: previousProject,
+            selectedBeneficiary: null,
             selectedProject: project
         });
     }
 
     render() {
+        let pageOutput = <h3>Witamy w programie Project Assistant. Proszę wybrać projekt lub beneficjenta</h3>
+        if (this.state.selectedBeneficiary !== null) {
+            pageOutput = <SelectedBeneficiary
+            selectedBeneficiary={this.state.selectedBeneficiary}
+            beneficiaryProjects={this.state.beneficiaryProjects} />
+        }
+
+        if(this.state.selectedProject !== null) {
+            pageOutput = <Project
+                name={this.state.selectedProject.name}
+                voivodeship={this.state.selectedProject.voivodeship} />
+        }
 
         return (
             <div className="row">
@@ -60,11 +87,9 @@ class ProjectManager extends Component {
                         clickBeneficiary={(beneficiary) => this.selectedBeneficiaryHandler(beneficiary)}
                         beneficiaries={this.state.beneficiaries}
                         clickProject={(project) => this.selectedProjectHandler(project)}
-                        />
+                    />
                 </div>
-                <SelectedBeneficiary
-                    selectedBeneficiary={this.state.selectedBeneficiary}
-                    beneficiaryProjects={this.state.beneficiaryProjects} />
+                {pageOutput}
             </div>
 
         )
