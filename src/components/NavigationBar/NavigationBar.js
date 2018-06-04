e52965bbad3a7c2cf784eb9d6927d7fbb5606e31
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-04 09:30:27 +0200

    clears class error

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -10,7 +10,7 @@ const navigationBar = () => (
         <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                    <a className="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+                    <a className="nav-link" href="">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="">Features</a>
