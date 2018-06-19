5cd29660762835e2e220abbb1e5437f5e0513dea
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-19 11:37:11 +0200

    adds route to selected project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 // import './App.css';
 
 import ProjectManager from './containers/ProjectManager/ProjectManager';
 import NavigationBar from './components/NavigationBar/NavigationBar';
+import selectedProject from './components/SelectedComponents/SelectedProject';
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
       <div className="App">
         <NavigationBar />
         <ProjectManager />
+        <Route path="manager/project/:id" exact component={selectedProject} />
       </div>
     );
   }
