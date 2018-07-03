8986eab563e2efc4646f67734c9c382a188c0fd7
mateuszlichon <mateusz.lichon@gmail.com>
2018-07-03 16:00:43 +0200

    adjusts views

--- src/components/NavigationPanel/ManagerNavigationPanel/ManagerNavigationPanel.js
+++ src/components/NavigationPanel/ManagerNavigationPanel/ManagerNavigationPanel.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import axios from '../../../axios-base';
 
 import ManagerNavigationBeneficiaries from './ManagerNavigationBeneficiaries/ManagerNavigationBeneficiaries';
+import { Button } from 'react-bootstrap';
+import AddBeneficiary from '../../AddFunction/AddBeneficiary/AddBeneficiary';
 
 class ManagerNavigationPanel extends Component {
     state = {
-        beneficiaries: []
+        beneficiaries: [],
+        addBeneficiary: false
     }
 
     componentDidMount() {
@@ -16,11 +19,28 @@ class ManagerNavigationPanel extends Component {
             });
     }
 
+    addProjectHandler = () => {
+        this.setState({ addBeneficiary: true });
+    }
+
+    backdropCancelHandler = () => {
+        this.setState({ addBeneficiary: false });
+    }
+
     render() {
+        let options = null;
+        if (this.state.addBeneficiary) {
+            options = <AddBeneficiary backdropCancel={this.backdropCancelHandler} />
+        }
+
         return (
-            <ManagerNavigationBeneficiaries
-                beneficiaries={this.state.beneficiaries}
-            />
+            <div>
+                {options}
+                <ManagerNavigationBeneficiaries
+                    beneficiaries={this.state.beneficiaries}
+                />
+                <Button block bsStyle="success" onClick={() => this.addProjectHandler()}>Dodaj beneficjenta</Button>
+            </div>
         )
     }
 }
