61bc55777e0183d4563819d69b8e89d197628856
mateuszlichon <mateusz.lichon@gmail.com>
2018-07-03 16:00:31 +0200

    develops delete functions

--- src/components/DeleteFunction/DeleteProject/DeleteProject.js
+++ src/components/DeleteFunction/DeleteProject/DeleteProject.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import Modal from '../../UI/Modal/Modal';
 import { Button } from 'react-bootstrap';
 
+import axios from '../../../axios-base';
+
 class DeleteProject extends Component {
+
+    submitDeleteHandler = (e) => {
+        e.preventDefault();
+        axios.delete('/projects/' + this.props.deleteProject.id)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+                
+            })
+            window.location.reload();
+    }
+
     render() {
         return (
             <Modal backdropCancel={this.props.backdropCancel}>
--- src/components/DeleteFunction/DeleteTask/DeleteTask.js
+++ src/components/DeleteFunction/DeleteTask/DeleteTask.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import Modal from '../../UI/Modal/Modal';
 import { Button } from 'react-bootstrap';
 
+import axios from '../../../axios-base';
+
 class DeleteTask extends Component {
+
+    submitDeleteHandler = (e) => {
+        e.preventDefault();
+        axios.delete('/tasks/' + this.props.deleteTask.id)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+                
+            })
+            window.location.reload();
+    }
+
     render() {
         return (
             <Modal backdropCancel={this.props.backdropCancel}>
